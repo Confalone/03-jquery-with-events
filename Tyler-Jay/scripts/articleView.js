08c975ad010dd8b1048499368a9c0b319d000b37
Jay Silvas <jaywsilvas@gmail.com>
2018-03-22 00:06:53 -0700

    Added Nav Functionality

--- Tyler-Jay/scripts/articleView.js
+++ Tyler-Jay/scripts/articleView.js
@@ -52,7 +52,7 @@ articleView.handleAuthorFilter = function() {
     }
     $('#category-filter').val('');
   });
-  $('article.template').hide(); // This may be extraneous, there's a DRY way to do it.
+  $('article.template').hide(); // This may be extraneous, there's a more DRY way to do it.
 };
 
 articleView.handleCategoryFilter = function() {
@@ -75,12 +75,17 @@ articleView.handleCategoryFilter = function() {
 };
 
 articleView.handleMainNav = function() {
-  // TODO: Add an event handler to .main-nav elements that will power the Tabs feature.
+  // TODOne: Add an event handler to .main-nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
+  $('nav [data-content="about"]').on('click', function() {
+    $('article').hide();
+  });
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-
   // REVIEW: Now trigger a click on the first .tab element, to set up the page.
-  $('.main-nav .tab:first').click();
+  $('.main-nav .tab:first').on('click', function() {
+    $('article').show();
+    $('article.template').hide();
+  });
 };
 
 articleView.setTeasers = function() {
@@ -88,6 +93,14 @@ articleView.setTeasers = function() {
   $('.article-body *:nth-of-type(n+2)').hide();
 
   // TODO: Add an event handler to reveal all the hidden elements, when the .read-on link is clicked. You can go ahead and hide the "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
+  $('article .read-on:focus').on('click', function() {
+    event.preventDefault();  
+    // TODOne: If the <select> menu was changed to an option that has a value, we first need to hide all the articles, and then show just the ones that match for the author that was selected.
+    // Use an "attribute selector" to find those articles, and fade them in for the reader
+    
+    $('this:parent').show();
+
+  });
   // Ideally, we'd attach this as just one event handler on the #articles section, and let it process (in other words... delegate) any .read-on clicks that happen within child nodes.
 };
 
@@ -96,5 +109,6 @@ $(document).ready(function() {
   articleView.populateFilters();
   articleView.handleAuthorFilter();
   articleView.handleCategoryFilter();
-
-})
+  articleView.handleMainNav();
+  // articleView.setTeasers();
+});
